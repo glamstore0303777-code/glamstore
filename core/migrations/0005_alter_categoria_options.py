69d9dccaeb829402e2863b942ff460e9dd6c751f
Lauren Ortiz <lausamanta2024cha@gmail.com>
2025-12-10 21:01:28 -0500

    Mover creación de tablas a migración inicial 0001

--- core/migrations/0005_alter_categoria_options.py
+++ core/migrations/0005_alter_categoria_options.py
@@ -10,8 +10,4 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AlterModelOptions(
-            name='categoria',
-            options={'managed': True},
-        ),
     ]
