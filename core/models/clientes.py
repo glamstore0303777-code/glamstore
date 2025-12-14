a0a74cbc4d5527714d0de2f52fd69d233fdc2092
Lauren Ortiz <lausamanta2024cha@gmail.com>
2025-12-14 11:26:39 -0500

    Fix: Remove cedula field from Cliente model - column doesn't exist in database

--- core/models/clientes.py
+++ core/models/clientes.py
@@ -2,7 +2,6 @@ from django.db import models
 
 class Cliente(models.Model):
     idCliente = models.AutoField(primary_key=True, db_column='idcliente')
-    cedula = models.CharField(max_length=20, db_column='cedula')
     nombre = models.CharField(max_length=100, null=True, db_column='nombre')
     email = models.CharField(max_length=100, unique=True, db_column='email')
     direccion = models.CharField(max_length=200, null=True, db_column='direccion')
