366b8fa8696ee5c993f97d35867df403c074b4f8
Lauren Ortiz <lausamanta2024cha@gmail.com>
2025-12-13 19:22:28 -0500

    Eliminar migración 0014 vacía y ajustar dependencia de 0015

--- core/migrations/0015_alter_repartidor_telefono.py
+++ core/migrations/0015_alter_repartidor_telefono.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0004_cliente_distribuidor_distribuidorproducto_repartidor_and_more'),
+        ('core', '0013_alter_detallepedido_options_alter_pedido_options_and_more'),
     ]
 
     operations = [
