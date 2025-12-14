7512b37f7bc3a2ce5c365e7ddd179c15d22bef39
Lauren Ortiz <lausamanta2024cha@gmail.com>
2025-12-13 22:11:25 -0500

    Add migration to sync models

---
+++ core/migrations/0042_sync_models.py
@@ -0,0 +1,13 @@
+# Generated migration to sync models with database
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0041_disable_foreign_keys'),
+    ]
+
+    operations = [
+    ]
