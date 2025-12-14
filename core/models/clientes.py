a944cc3266158104ff28548de73890b263481981
Lauren Ortiz <lausamanta2024cha@gmail.com>
2025-12-14 12:31:31 -0500

    Arreglo: Agregar campos faltantes al modelo Cliente (nombre, email, telefono, etc.)

--- core/models/clientes.py
+++ core/models/clientes.py
@@ -2,8 +2,20 @@ from django.db import models
 
 class Cliente(models.Model):
     idCliente = models.AutoField(primary_key=True, db_column='idcliente')
+    nombre = models.CharField(max_length=100, null=True, blank=True, db_column='nombrecliente')
+    apellido = models.CharField(max_length=100, null=True, blank=True, db_column='apellidocliente')
+    email = models.EmailField(max_length=100, null=True, blank=True, db_column='emailcliente')
+    telefono = models.CharField(max_length=20, null=True, blank=True, db_column='telefonocliente')
+    direccion = models.TextField(null=True, blank=True, db_column='direccioncliente')
+    ciudad = models.CharField(max_length=50, null=True, blank=True, db_column='ciudadcliente')
+    departamento = models.CharField(max_length=50, null=True, blank=True, db_column='departamentocliente')
+    codigo_postal = models.CharField(max_length=20, null=True, blank=True, db_column='codigopostalcliente')
+    cedula = models.CharField(max_length=20, null=True, blank=True, db_column='cedula')
 
     class Meta:
         db_table = 'clientes'
-        managed = True  # Django maneja la creación y actualización de esta tabla
+        managed = False  # No dejar que Django maneje la tabla, ya existe en BD
         app_label = 'core'
+    
+    def __str__(self):
+        return f"{self.nombre} ({self.idCliente})" if self.nombre else f"Cliente {self.idCliente}"
