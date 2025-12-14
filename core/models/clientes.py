9a7b47461dd47172f0d3332a15a155108e1bb6a3
Lauren Ortiz <lausamanta2024cha@gmail.com>
2025-12-14 11:34:14 -0500

    Fix: Remove non-existent Cliente fields (nombre, cedula) from model to match PostgreSQL schema

--- core/models/clientes.py
+++ core/models/clientes.py
@@ -2,10 +2,7 @@ from django.db import models
 
 class Cliente(models.Model):
     idCliente = models.AutoField(primary_key=True, db_column='idcliente')
-    nombre = models.CharField(max_length=100, null=True, db_column='nombre')
-    email = models.CharField(max_length=100, unique=True, db_column='email')
-    direccion = models.CharField(max_length=200, null=True, db_column='direccion')
-    telefono = models.CharField(max_length=20, null=True, db_column='telefono')
+    email = models.CharField(max_length=100, unique=True, db_column='email', null=True, blank=True)
 
     class Meta:
         db_table = 'clientes'
