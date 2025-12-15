8b6f0a492fd458dfbf41a2361e27491659b3ad87
Lauren Ortiz <lausamanta2024cha@gmail.com>
2025-12-14 23:13:51 -0500

    Arreglos finales: Notificaciones, Movimientos y Productos

--- core/migrations/0078_add_missing_notificaciones_columns.py
+++ core/migrations/0078_add_missing_notificaciones_columns.py
@@ -11,36 +11,10 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunSQL(
-            sql="""
-            CREATE TABLE IF NOT EXISTS notificaciones_problema_new (
-                idnotificacion INTEGER PRIMARY KEY AUTOINCREMENT,
-                idpedido BIGINT NOT NULL,
-                motivo TEXT,
-                foto VARCHAR(100),
-                fechareporte DATETIME DEFAULT CURRENT_TIMESTAMP,
-                leida BOOLEAN DEFAULT 0,
-                respuesta_admin TEXT,
-                fecha_respuesta DATETIME,
-                FOREIGN KEY (idpedido) REFERENCES pedidos(idpedido)
-            );
-            
-            INSERT INTO notificaciones_problema_new 
-            SELECT 
-                idnotificacion,
-                idpedido,
-                motivo,
-                foto,
-                COALESCE(fechareporte, CURRENT_TIMESTAMP),
-                leida,
-                respuesta_admin,
-                fecha_respuesta
-            FROM notificaciones_problema;
-            
-            DROP TABLE notificaciones_problema;
-            
-            ALTER TABLE notificaciones_problema_new RENAME TO notificaciones_problema;
-            """,
-            reverse_sql="",
+        migrations.AddField(
+            model_name='notificacionproblema',
+            name='fechaReporte',
+            field=models.DateTimeField(auto_now_add=True, db_column='fechareporte', default=django.utils.timezone.now),
+            preserve_default=False,
         ),
     ]
